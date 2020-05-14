32a0d8df4457838cc0c38f5b45c592557c85d01c
JaimeRamos99 <jrossetes@uninorte.edu.co>
2020-05-14 02:24:00 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const https = require("https");
+setTimeout(function () {
+  https
+    .get(process.env.url, (resp) => {
+      let data = "";
+
+      resp.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      resp.on("end", () => {
+        console.log(data);
+      });
+    })
+    .on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
+}, 60000);

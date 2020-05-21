5fef84bed8c3e51a46d78f28ab9a9c1dcaa30c1a
JaimeRamos99 <jrossetes@uninorte.edu.co>
2020-05-21 16:46:25 -0500

    trying cron

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-const https = require("https");
-setInterval(function () {
-  https
-    .get('https://holuserver.herokuapp.com/probando', (resp) => {
-      let data = "";
-
-      resp.on("data", (chunk) => {
-        data += chunk;
-      });
-
-      resp.on("end", () => {
-        console.log(data);
-      });
-    })
-    .on("error", (err) => {
-      console.log("Error: " + err.message);
-    });
-}, 60000);
+const request = require("request");
+const CronJob = require("cron").CronJob;
+const job = new CronJob(
+  "0 */25 * * * *",
+  function () {
+    request(
+      {
+        url: "https://holuserver.herokuapp.com/probando",
+        method: "GET",
+        timeout: 10000,
+        followRedirect: true,
+        maxRedirects: 10,
+      },
+      function (error, response, body) {
+        if (!error) {
+          console.log("sucess!");
+        } else {
+          console.log("error " + response.statusCode);
+        }
+      }
+    );
+  },
+  null,
+  true,
+  "America/Los_Angeles"
+);
+job.start();
